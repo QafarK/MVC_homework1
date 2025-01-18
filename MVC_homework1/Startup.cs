3a6165961f8e40b607dda1cda1bad77859777867
QafarK <mirqafar2004@gmail.com>
2025-01-18 11:25:39 +0400

    startup

--- MVC_homework1/Startup.cs
+++ MVC_homework1/Startup.cs
@@ -50,7 +50,7 @@ namespace MVC_homework1
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Drink}/{action=Index}/{id?}");
             });
         }
     }
